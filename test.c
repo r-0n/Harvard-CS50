e80776963960f08c087797feba3b9004922dc217
r-0n <ronywajah911@gmail.com>
2023-07-02 11:48:20 +0000

    starter

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "cs50.h"
+#include<stdio.h>
+
+
+int main(void)
+{
+    for(int i=1; i<=4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+    printf("\n");
+
+    for(int j=1; j<=4; j++)
+    {
+        printf(" __\n");
+        printf("|__| \n");
+    }
+}
